bdeb4dbc5f20b9303418d778a397a49321c924b8
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-17 15:03:43 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
 def starts_with_a_vowel?(word)
-
-end
+  if word.match (/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
+  
+  end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan (/un\w+ing/)
 
 end
 
 def words_five_letters_long(text)
+  text.scan (/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match (/\A{[A-Z]\w+\W\b/)
 
 end
 
 def valid_phone_number?(phone)
+  
+  end
 
 end

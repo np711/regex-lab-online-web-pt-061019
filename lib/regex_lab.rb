ad25fc04f9847c7af71871d1104803ea32ba0e5a
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-17 14:35:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
 def starts_with_a_vowel?(word)
-  if word.match (/^[AEIOU]|^)
+  if word.match (/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
   
-  
-
-end
+  end
 
 def words_starting_with_un_and_ending_with_ing(false)
 
